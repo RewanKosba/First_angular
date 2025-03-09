4c3848b026dc656cd5ab0530f9838262466c0692
Rewan Kosba <rewankosba58@gmail.com>
2025-03-09 22:55:51 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports:[ HeroComponent , AboutComponent , SkillsComponent , PortfolioComponent , FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
